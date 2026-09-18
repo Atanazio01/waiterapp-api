5f409f31a8ac246a86ebe845ee010075447dce0f
Atanazio01 <marcosd.atanazio@gmail.com>
2026-09-18 16:58:24 -0300

    Adding order endpoints and switching production build to tsup

---
+++ src/app/useCases/orders/cancelOrder.ts
@@ -0,0 +1,15 @@
+import type { Request, Response } from "express";
+import { Order } from '../../models/Order';
+
+export const cancelOrder = async (req: Request, res: Response) => {
+  try {
+    const { orderId } = req.params;
+
+    await Order.findByIdAndDelete(orderId);
+
+    res.sendStatus(204);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
---
+++ src/app/useCases/orders/listOrders.ts
@@ -0,0 +1,14 @@
+import type { Request, Response } from "express";
+import { Order } from "../../models/Order";
+
+export const listOrders = async (req: Request, res: Response) => {
+  try {
+    const orders = await Order.find()
+      .sort({ createdAt: -1 })
+      .populate("products.product");
+    res.status(200).json(orders);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
