14b53cf656655734c22d6f93327e1a02273206e6
Atanazio01 <marcosd.atanazio@gmail.com>
2026-09-18 10:27:09 -0300

    Defining database models with Mongoose

---
+++ src/app/models/Product.ts
@@ -0,0 +1,51 @@
+import { model, Schema } from "mongoose";
+
+export const Product = model(
+  "Product",
+  new Schema({
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    imagePath: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    ingredients: {
+      type: [
+        {
+          name: {
+            type: String,
+            required: true,
+          },
+          icon: {
+            type: String,
+            required: true,
+          },
+        },
+      ],
+      required: true,
+    },
+    category: {
+      type: Schema.Types.ObjectId,
+      ref: "Category",
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    updatedAt: {
+      type: Date,
+      default: Date.now,
+    },
+  }),
+);
